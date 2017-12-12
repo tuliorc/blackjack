087e0f6d4a3cdf1629c740ba876434aa8ceca44a
Tulio Carreira <trcarreira@gmail.com>
2017-12-12 11:28:01 -0200

    Define hit, stand and get_bet_input functions
    - Change ace representation from "ace" to "A"
    - Define hit() and stand() functions
    - Define get_bet_input() function
    - Create main loop for playing different rounds
    - Set bankroll subtraction for when player lost the game

--- blackjack.py
+++ blackjack.py
@@ -27,6 +27,10 @@ class Player():
         print("{name}'s hand: ".format(name=self.name))
         self.hand.print_cards()
 
+    def print_bankroll(self):
+        print("{name}'s money: {bankroll}".format(name=self.name, 
+                                                    bankroll=self.bankroll))
+
     def show_all_cards(self):
         for card in self.hand.cards:
             card.hidden = False
@@ -40,24 +44,23 @@ class Card():
         if isinstance(self.rank, int):
             # if the rank is a numeric value, points will correspond to it
             self.points = self.rank
-        elif self.rank == "ace":
+        elif self.rank == "A":
             # if the rank is an Ace, points can be either 1 or 11
-            self.points = (1, 11)
+            self.points = 1
         else:
             # if the rank is a Queen, King, or Jack, points are equal to 10
             self.points = 10
 
     def __str__(self):
         if not self.hidden:
-            return "[{suit}{rank}] ({points} points)".format(
-                suit=suit_dict[self.suit], rank=self.rank, points=self.points)
+            return "[{suit}{rank}]".format(suit=suit_dict[self.suit], 
+                                           rank=self.rank)
         return "[???]"
 
 
 class Hand():
-    def __init__(self, cards, type_hand):
+    def __init__(self, cards):
         self.cards = []
-        self.type_hand = type_hand
         self.value = 0
         self.ace = False # there is no ace in hand
         for card in cards:
@@ -65,7 +68,7 @@ class Hand():
 
     def add_card(self, card):
         self.cards += [card]
-        if card.rank = "ace":
+        if card.rank == "A":
             self.ace = True
         self.value += card.points
 
@@ -82,7 +85,7 @@ class Hand():
 
 def generate_deck():
     suits = ['club', 'diamond', 'heart', 'spade']
-    ranks = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
+    ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
     cards = []
     for suit in suits:
         for rank in ranks:
@@ -90,52 +93,70 @@ def generate_deck():
     return cards
 
 
-def get_card_from_deck(deck):
+def get_card_from_deck():
     return deck.pop(random.randrange(0, len(deck)))
 
-def play_round(player, dealer):
-    option_input = None
-    while not isinstance(option_input, int) or option_input not in [1, 2]:
+def hit():
+    if human.hand.total_points() <= 21:
+        human.hand.add_card(get_card_from_deck())
+    if human.hand.total_points() > 21:
+        human.subtract_from_bankroll(bet)
+
+def stand():
+    dealer.show_all_cards()
+    while dealer.hand.total_points() < 17:
+        dealer.hand.add_card(get_card_from_deck())
+        if dealer.hand.total_points() >= 21:
+            return "dealer lost"
+
+def get_bet_input():
+    try:
+        bet_input = int(input("How much do you want to bet?"))
+        if 0 < bet_input < human.bankroll:
+            return bet
+        return None
+    except:
+        return None
+
+
+global deck, game_is_on, human, dealer
+deck = generate_deck()
+human = Player("Player1", Hand([get_card_from_deck(),
+                                get_card_from_deck()]))
+dealer = Player("PC", Hand([get_card_from_deck(),
+                            get_card_from_deck()]), True)
+
+
+game_is_on = True
+while game_is_on and human.bankroll > 0:
+    human.print_hand()
+    human.print_bankroll()
+    print("---------------------")
+    dealer.print_hand()
+    option_input, bet_input = None, None
+    while option_input not in ['s', 'h', 'q']:
         try:
-            option_input = int(
-                input("Type 1 for hitting or 2 for standing: "))
+            option_input = str(input("Type 'h' for hitting, " + \
+                                 "'s' for standing or 'q' for quitting: "))
         except:
             continue
-        if option_input == 1:
-            player.hand.add_card(get_card_from_deck(deck))
-            while player.hand.total_points
-            is_hand_busted(player):
-                print("jogador perdeu!")
-                pass
-        elif option_input == 2:
-            dealer.show_all_cards()
-            if not is_hand_busted(dealer):
-
-            pass
+        if option_input == 'q':
+            game_is_on = False
+            break
         else:
-            continue
+            bet = get_bet_input()
+            if not bet:
+                continue
+            else:
+                if option_input == 'h':
+                    hit(bet)
+                if option_input == 's':
+                    stand(bet)
+    else:
+        continue
 
-
-def is_hand_busted(player):
-    if player.isdealer and player.hand.total_points > 17:
-        # dealer cannot pass 17 points
-        return True
-    if not player.isdealer and player.hand.total_points >= 21:
-        # human player cannot go over 21 points
-        return True
-    return False
-
-
-global deck
-deck = generate_deck()
-human = Player("Player1", Hand([get_card_from_deck(deck),
-                                get_card_from_deck(deck)]))
-dealer = Player("PC", Hand([get_card_from_deck(deck),
-                            get_card_from_deck(deck)]), True)
-
-human.print_hand()
-print("---------------------")
-dealer.print_hand()
-
-play_round(human, dealer)
-
+else:
+    print("----------------------------")
+    print("Game over! Hope you had fun!")
+    print(human.print_bankroll())
+    print("----------------------------")
